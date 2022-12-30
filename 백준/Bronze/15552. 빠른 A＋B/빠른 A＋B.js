a6d75e9c8870664884a58326e016c64a58b2980e
usiyoung <usiyoung7@gmail.com>
2022-12-30 21:30:55 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1244 ms, Memory: 170612 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+let result = '';
+for(let i = 1; i <= input[0]; i++){
+  const newArr = input[i].split(' ');
+  result += Number(newArr[0]) + Number(newArr[1]) + '\n';
+}
+
+console.log(result)
