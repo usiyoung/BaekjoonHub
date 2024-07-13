27265d791c721f718b81aaebfabb7c97f6018462
usiyoung <usiyoung7@gmail.com>
2024-07-13 15:20:55 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> A(N, 0);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - 1 - i; j++) {
+            if(A[j] > A[j + 1]){
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < N; ++i) {
+        cout << A[i] << '\n';
+    }
+    return 0;
+}
